4585506eec30798a755b74017d6ce51f9275193d
evgeniy <eugendzygew@gmail.com>
2021-12-24 11:04:40 +0300

    all ready

--- app/js/main.js
+++ app/js/main.js
@@ -4,23 +4,17 @@ let container = document.querySelector('#container')
 const mainForm = document.querySelector('#mainform')
 const updateBtn = document.querySelector('#update')
 const closeModalBtn = document.querySelector('#close-modal-window')
-// const arr = []
+const arrtimeoutID = []
 
-const newElement = function (){
-
-
-
-}
-
-sessionStorage.clear()
+ sessionStorage.clear()
 
 const request = function (event) {
   event.preventDefault();
   
   let city = document.querySelector('#city').value
-  let key = '5242e56a881e81ef9e3e59278ec7189d'
+  let key = '4c5044519b3b54a220c234bfd2b5d6b4'
 
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},london&appid=${key}`)
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
   .then(function(resp){
     //обрабатываем успешно выполненный запрос и преобразует его в json формат(строку преобразует в массив)
     return resp.json()})
@@ -49,10 +43,26 @@ const request = function (event) {
     function newElement(cityName, degrees, description, icon){
       let newCard = document.createElement('div')
       newCard.classList.add('meteo-card')
-      newCard.innerHTML = `<span class="meteo-card__close"></span><h2>${cityName}</h2><p>${degrees}</p><p>${description}
+      newCard.innerHTML = `<span class="meteo-card__close"></span><h2>${cityName}</h2><p class="degrees">${degrees}</p><p class="description">${description}
       </p><img src="" >` 
       newCard.querySelector('img').setAttribute('src', `https://openweathermap.org/img/wn/${icon}.png`);
       container.append(newCard)
+
+      const timeoutID =  setTimeout(function update(){
+                          console.log('рекурсивный сетТаймаут запущен');
+                          arrtimeoutID.push(timeoutID)
+                          console.log(arrtimeoutID);
+                          updateShow()
+                          setTimeout(update,10000);
+                          },10000);
+      console.log(timeoutID);
+      
+/*       setTimeout(function update(){
+        console.log('рекурсивный сетТаймаут запущен');
+        updateShow()
+        setTimeout(update,1000);
+      },1000) */
+
     }
 
   }).then (function(){
@@ -75,25 +85,63 @@ const request = function (event) {
 
 
 function updateShow(){
-  console.log('обновить');
+  let key = '4c5044519b3b54a220c234bfd2b5d6b4'
+  const arr = JSON.parse(sessionStorage.getItem('newArr'));
+  console.log(arr);
+  for (const item in arr) {
+    console.log(arr[item]);
+    let city = arr[item]
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},london&appid=${key}`)
+    .then(function(resp){
+      //обрабатываем успешно выполненный запрос и преобразует его в json формат(строку преобразует в массив)
+      return resp.json()})
+    .then(function(data){
+      console.log(data);
+      for(let i = 0; i<container.children.length; i++){
+        if(i == item){
+          let degrees = Math.round(data.main.temp - 273,15) +'&deg';
+          let description = data.weather[0]['description'];
+          let icon = data.weather[0]['icon'];         
+          console.log(container.children[i]);
+          container.children[i].querySelector('.degrees').innerHTML = degrees;
+          container.children[i].querySelector('.description').innerHTML = description;
+          container.children[i].querySelector('img').setAttribute('src',`https://openweathermap.org/img/wn/${icon}.png`)
+        }
+      }        
+   
+    }).then (function(){
+
+    })
+    .catch(function(error){
+      //обрабатываем ошибку
+      console.log(error);
+      console.log('error');
+    })
+    .then(function(){
+      //выполняется всегда
+  
+    })
+  }
+  // if(){
+
+  // }
+  // fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},london&appid=${key}`)
 }
 const closeCard = function(event){
   if(event.target.classList.contains('meteo-card__close')){
     console.log(event.target.nextSibling.innerHTML);
     const arr = JSON.parse(sessionStorage.getItem('newArr'));
 
-    // for(let i = 0; i<arr.length; i++){
-    //   if(arr[i] === event.target.nextSibling.innerHTML)
-    //   console.log(arr[i]);
-    //   arr.splice(i,1)
-    //   console.log(arr);
-    //   break
-    // }
+    for(let i = 0; i<arr.length; i++){
+      if(arr[i] === event.target.nextSibling.innerHTML) {
+        console.log(arr[i]);
+        arr.splice(i,1)
+        console.log(arr);
+        sessionStorage.setItem('newArr', JSON.stringify(arr))
+        event.target.closest('.meteo-card').remove()
+      }
+    }
 
-    const index = arr.indexOf(el => el === event.target.nextSibling.innerHTML);
-    arr.splice(index, 1)
-    sessionStorage.setItem('newArr', JSON.stringify(arr))
-    event.target.closest('.meteo-card').remove()  
   }
   if(!container.firstChild){
     updateBtn.classList.remove('active')
